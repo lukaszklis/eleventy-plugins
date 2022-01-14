c0b4c5a6bebcf3106e775a8b057ceb5bd2df58ff
Hugo Nogueira <hnogueira@marabyte.com>
2022-01-14 19:52:21 +0000

    chore: Fix test and clean up

--- packages/tinycss/lib/options.interface.ts
+++ packages/tinycss/lib/options.interface.ts
@@ -1,9 +1,6 @@
-import { Options as AutoprefixerOptions } from 'autoprefixer';
 import { PurgeCSSOptions } from './purgeCSS.interface';
 
 export interface OptionsInterface {
-  /** Autoprefixer options */
-  autoprefixer?: AutoprefixerOptions;
   /** The 11ty output directory. It defaults to `_site` */
   output?: string;
   purgeCSS?: PurgeCSSOptions;
